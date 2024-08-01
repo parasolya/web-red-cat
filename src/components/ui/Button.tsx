6e52b5466f6d3f5e4c92f280ccec7035b8480152
parasolya <ola.shevchuk@gmail.com>
2024-08-01 11:59:30 +0300

    added className to styles of Button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -10,13 +10,14 @@ const Button: React.FC<ButtonProp> = ({
 }) => {
   return (
     <button
-      className={clsx(
-        'text-primary uppercase outline-none shadow-none cursor-pointer hover:underline underline-offset-4 transition',
-        variant === 'slider' && 'font-thin text-2xl text-primary',
-        variant === 'text' && 'text-4xl  md:text-3xl lg:text-3xl decoration-2'
-      )}
-      {...rest}
-    >
+    className={clsx(
+      'text-primary uppercase outline-none shadow-none cursor-pointer hover:underline underline-offset-4 transition',
+      variant === 'slider' && 'font-thin text-2xl text-primary',
+      variant === 'text' && 'text-4xl md:text-3xl lg:text-3xl decoration-2',
+      className // Додаємо className тут
+    )}
+    {...rest}
+  >
       {children}
     </button>
   );
