b88e1eaa0396f384448805410a47808f20ce27e7
parasolya <ola.shevchuk@gmail.com>
2024-08-06 23:46:56 +0300

    added styles to ProjecstItem

--- src/components/common/ProjectsItem/ProjectsItem.tsx
+++ src/components/common/ProjectsItem/ProjectsItem.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 
 const ProjectsItem: React.FC<ProjectsItemProps> = ({ src, alt}) => {
   return (
-    <div className='w-full relative h-[229px] overflow-hidden sm:w-[448px] md:h-[274px] md:w-[330px] lg:w-[521px] '>
+    <div className='w-full relative h-[229px] overflow-hidden sm:w-[448px] md:h-[320px] md:w-[330px] lg:w-[560px] '>
        <Image
               className='object-cover'
               fill
               src={src}
               alt={alt}
-              sizes='(max-width: 768px) 448px, (max-width: 1280px) 330px, 521px'
+              sizes='(max-width: 768px) 448px, (max-width: 1280px) 330px, 560px'
               priority
             />
     </div>
