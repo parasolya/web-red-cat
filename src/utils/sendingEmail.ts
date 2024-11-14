a5320731c1d8dd2bf75997278614adf252436f9b
parasolya <ola.shevchuk@gmail.com>
2024-11-14 19:01:26 +0200

    fixed path api

--- src/utils/sendingEmail.ts
+++ src/utils/sendingEmail.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { IFormValues } from '@/@types';
 
 const sendingEmail = async (formData: IFormValues): Promise<void> => {
-  await axios.post('/api/nodemailer', formData);
+  await axios.post('/api/contact', formData);
 };
 
 export default sendingEmail;
